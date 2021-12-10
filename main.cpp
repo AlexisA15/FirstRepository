9d1b65aa50cd8c15c2e7b60fedcb2633acbdc947
AlexisA15 <95914817+AlexisA15@users.noreply.github.com>
2021-12-10 05:10:01 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,6 @@
+int main() {
+	cout << two::counter << endl;
+	cout << "It is nice to edit in Windows using VI" << endl;
+	cout <<  "This is Alexis Acevedo!" << endl;
+	cout <<  "This is John Cena!" << endl;
+}
